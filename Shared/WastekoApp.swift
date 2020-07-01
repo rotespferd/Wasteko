645d9def64fb21fe14818382aa4a4e2c2efbf641
Marian Hahne <marian.hahne@outlook.com>
2020-07-01 18:18:53 +0200

    Initial Commit

---
+++ Shared/WastekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WastekoApp.swift
+//  Shared
+//
+//  Created by Marian Hahne on 01.07.20.
+//
+
+import SwiftUI
+
+@main
+struct WastekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
